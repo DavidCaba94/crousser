d1f260aa408fbd2703378057baf57b6ff89e75eb
DavidCaba6194 <cabayosoyasi@gmail.com>
2020-07-16 20:09:25 +0200

    Como funciona y pestañas

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
+    $("#tab-webmaster").on("click", function() {
+        $("#tab-webmaster").addClass("activa");
+        $("#tab-negocio").removeClass("activa");
+    });
 
+    $("#tab-negocio").on("click", function() {
+        $("#tab-negocio").addClass("activa");
+        $("#tab-webmaster").removeClass("activa");
+    });
 });
