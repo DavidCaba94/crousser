d96317830140e3ad7be887c06b115d5d0d651386
DavidCaba6194 <cabayosoyasi@gmail.com>
2020-07-20 21:24:02 +0200

    Sección de tips y de precios

--- js/index.js
+++ js/index.js
@@ -2,10 +2,14 @@ $(document).ready(function(){
     $("#tab-webmaster").on("click", function() {
         $("#tab-webmaster").addClass("activa");
         $("#tab-negocio").removeClass("activa");
+        $("#tips-webmaster").css("display", "block");
+        $("#tips-negocio").css("display", "none");
     });
 
     $("#tab-negocio").on("click", function() {
         $("#tab-negocio").addClass("activa");
         $("#tab-webmaster").removeClass("activa");
+        $("#tips-negocio").css("display", "block");
+        $("#tips-webmaster").css("display", "none");
     });
 });
