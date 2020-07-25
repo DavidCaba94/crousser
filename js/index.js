ed13cb904fe6fc8991ba2d00b11494d1dd8cfe89
DavidCaba6194 <cabayosoyasi@gmail.com>
2020-07-25 21:10:00 +0200

    Preguntas y respuestas y capa footer

--- js/index.js
+++ js/index.js
@@ -12,4 +12,27 @@ $(document).ready(function(){
         $("#tips-negocio").css("display", "block");
         $("#tips-webmaster").css("display", "none");
     });
+
+    $(".pregunta").on("click", function() {
+        if($(this).find('.flecha-desplegable').hasClass("desplegada")){
+            rotarFlecha(this, 0);
+            $(this).find('.flecha-desplegable').removeClass("desplegada");
+        } else {
+            rotarFlecha(this, -180);
+            $(this).find('.flecha-desplegable').addClass("desplegada");
+        }
+        $(this).next().slideToggle();
+    });
 });
+
+function rotarFlecha(flecha, direccion){
+    $(flecha).find('.flecha-desplegable').animate(
+        { deg: direccion },
+        {
+            duration: 500,
+            step: function(now) {
+                $(this).css({ transform: 'rotate(' + now + 'deg)' });
+            }
+        }
+    );
+}
