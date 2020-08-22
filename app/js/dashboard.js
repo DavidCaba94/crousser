6db9424a4d92df0928dbec67deb5fc64e9493595
MSI\david <david.caballero94@hotmail.com>
2020-08-22 20:48:53 +0200

    Dashboard con toolbar y navigation estable

--- app/js/dashboard.js
+++ app/js/dashboard.js
@@ -1,13 +1,38 @@
+var usuario;
+
 $(document).ready(function(){
-	var usuario = localStorage.getItem('userObject');
+	usuario = localStorage.getItem('userObject');
 
 	if(JSON.parse(usuario) == null) {
 		window.location.href = 'https://crousser.com/app/login';
-    }
+    } else {
+		usuario = JSON.parse(usuario);
+		console.log(usuario);
+		$("#nav-nombre").text(usuario.nombre);
+		$("#nav-apellidos").text(usuario.apellidos);
+		if(usuario.foto != null){
+			$("img-usuario").attr("src", usuario.foto);
+			$("img-usuario-nav").attr("src", usuario.foto);
+		}
+		if(usuario.vip == 1){
+			$(".marco-imagen").css("background-color", "#ffcf00");
+			$(".marco-imagen-nav").css("background-color", "#ffcf00");
+		}
+	}
 
 	$('#btn-cerrar-sesion').on('click', function() {
 		localStorage.setItem('userObject', null);
 		localStorage.setItem('checkRecordar', null);
 		window.location.href = 'https://crousser.com/app/login';
+	});
+
+	$('#btn-cerrar-sesion-nav').on('click', function() {
+		localStorage.setItem('userObject', null);
+		localStorage.setItem('checkRecordar', null);
+		window.location.href = 'https://crousser.com/app/login';
+	});
+
+	$('#img-toggle').on('click', function() {
+		$('#navigation-menu').animate({width: 'toggle'});
 	});
 });
